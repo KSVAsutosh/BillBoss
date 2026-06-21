3d4f830fa8eb04f9f91940111babbb5e9aad252b
Kappagantu Sai Venkata Asutosh <appuasutosh90@gmail.com>
2026-06-21 08:52:35 +0530

    Production settings

--- billboss/settings.py
+++ billboss/settings.py
@@ -1,12 +1,22 @@
 from pathlib import Path
+import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-billboss-secret-key'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    ...
+]
 
 INSTALLED_APPS = [
 
